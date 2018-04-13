d274e6601a34c6304681d5c6f633ca0b9a177909
Cees-Jan Swart <cj.swart@quicknet>
2018-04-14 00:11:38 +0200

    fix rspec error

---
+++ spec/versions.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+SUPPORTED_FACTS = [
+  { 'os' => {
+    'name' => 'CentOs',
+    'family' => 'RedHat',
+    'release' => {
+      'major' => '7',
+      'minor' => '2',
+      'full' => '7.2.1511',
+    },
+  } },
+  { 'os' => {
+    'name' => 'CentOs',
+    'family' => 'RedHat',
+    'release' => {
+      'major' => '6',
+      'minor' => '3',
+      'full' => '6.3.834',
+    },
+  } },
+].freeze
+UNSUPPORTED_FACTS = [
+  { 'os' => {
+    'name' => 'windows',
+    'family' => 'windows',
+    'release' => {
+      'major' => '2012 R2',
+      'minor' => '2012',
+      'full' => '2012 R2',
+    },
+  } },
+].freeze
